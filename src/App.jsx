c39cf7294c5a8d3859dcbc59527c4dcb04dbaca2
alenarejmond-hash <alenarejmond@gmail.com>
2026-04-13 03:36:45 +0300

    путь к фото

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const CONTENT = {
   // 🇷🇺 РУССКИЙ ЯЗЫК
   ru: {
     creator: {
-      bgImage: '/bg-creator.jpg',
+      bgImage: 'bg-creator.jpg',
       avatar: '/avatar-creator.jpg', 
       audioGreeting: '/greeting.mp3', // 🔊 Ссылка на ваш аудиофайл (можно сделать разные для RU и EN)
       badge: 'TRANSFORMATION',
