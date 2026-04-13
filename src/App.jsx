641c67d5ef0ca8053898d40ebe620230552554f1
alenarejmond-hash <alenarejmond@gmail.com>
2026-04-13 22:36:29 +0300

    правка иконки для приложения

--- src/App.jsx
+++ src/App.jsx
@@ -962,11 +962,20 @@ const App = () => {
         display: "standalone",
         background_color: "#0a0a0a",
         theme_color: "#059669",
-        icons: [{
-          src: CONTENT[lang].creator.avatar || "https://api.qrserver.com/v1/create-qr-code/?size=192x192&data=PWA",
-          sizes: "192x192",
-          type: "image/png"
-        }]
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
       };
       const stringManifest = JSON.stringify(manifest);
       const blob = new Blob([stringManifest], { type: 'application/json' });
