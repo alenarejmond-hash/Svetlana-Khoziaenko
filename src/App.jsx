ca4648dcaa3f5102b96bcfa120c6411b9e700557
alenarejmond-hash <alenarejmond@gmail.com>
2026-04-14 23:30:09 +0300

    Reapply "Я добавила специальную системную команду orientation: "portrait" в настройки приложения (в Canvas / правое окно). Теперь, когда визитка установлена на экран «Домой» (как приложение), телефон будет жестко держать её в вертикальном положении, даже если его повернуть набок!"
    This reverts commit f259d0b384c2546b2d3f25ffe908a6bef47b3675.

--- src/App.jsx
+++ src/App.jsx
@@ -1004,6 +1004,7 @@ const App = () => {
         display: "standalone",
         background_color: "#0a0a0a",
         theme_color: "#059669",
+        orientation: "portrait", // Добавлена блокировка ориентации
         icons: [
           {
             src: "/icon-192.png",
