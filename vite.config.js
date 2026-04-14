8dddb1564800b2af8440cfa03f6bd8a5daa20ffc
alenarejmond-hash <alenarejmond@gmail.com>
2026-04-15 00:25:10 +0300

    внесли изменения в vite.config.js и в файле прописали функция для убирания белого окна

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // ВАЖНО! Если у тебя СВОЙ ДОМЕН (sotnikova.pro), пиши base: '/',
-  // Если у тебя БЕСПЛАТНАЯ ссылка Гитхаба, пиши base: '/НазваниеРепозитория/', 
-  base: '/', 
+  base: '/', // Оставляем твою настройку для своего домена!
+  plugins: [
+    react(), 
+    tailwindcss(), // Оставляем твой Tailwind!
+    VitePWA({
+      registerType: 'autoUpdate', // Автоматически обновляет SW в фоне
+      workbox: {
+        cleanupOutdatedCaches: true, // Убивает старый кэш
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,mp3}']
+      }
+    })
+  ],
 })
